eca13bbc84d0bc040feaa27a86a4c6aede849110
Mikhail Vasiliev <m89154280096@yandex.ru>
2024-02-25 21:54:23 +0300

    fixed current_time

--- test_smth.py
+++ test_smth.py
@@ -7,7 +7,7 @@ def test_dark_theme_by_time():
     """
     current_time = time(hour=23)
     # переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
-    if current_time in range(6, 22):
+    if current_time.hour in range(6, 22):
         is_dark_theme = False
     else:
         is_dark_theme = True
@@ -29,7 +29,7 @@ def test_dark_theme_by_time_and_user_choice():
     #  но учтите что темная тема может быть включена вручную
 
     if dark_theme_enabled_by_user is None:
-        if current_time not in range(6, 22):
+        if current_time.hour not in range(6, 22):
             is_dark_theme = True
         else:
             is_dark_theme = False
